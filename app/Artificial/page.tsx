d8c9726027c7057e4126cca115984a9749fa9efd
syedshahmirsultan <115161841+syedshahmirsultan@users.noreply.github.com>
2023-04-18 23:04:28 +0500

    Update page.tsx

--- app/Artificial/page.tsx
+++ app/Artificial/page.tsx
@@ -4,7 +4,7 @@ export default function Artificial(){
     return(
         <div>
         <div className="h-[800px] w-2/2 bg-blue-900">
-        <h1 className="text-[85px] ml-[240px] text-white pt-[230px] font-extrabold">Artificial Intelligence</h1>
+        <h1 className="text-[85px] ml-[240px] text-white pt-[130px] font-extrabold">Artificial Intelligence</h1>
         <text className="pt-[10px] text-[25px] ml-[240px] text-white font-extrabold">A ONE YEAR AI PROGRAM DESIGNED FOR ABSOLUTE BEGINNERS.</text>
 <br/><text className="text-[25px] ml-[220px] text-white font-extrabold">GETTING PAKISTAN READY FOR THE NEW ERA OF COMPUTING ENABLED BY THE RISE OF AI.</text>
        <div className="ml-[-50px] mt-[150px]"><Button><Link href="https://portal.piaic.org/signup">Enroll now</Link></Button></div></div>
