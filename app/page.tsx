ce60688a9a1f42656cda3bb2038fba53d4eae17c
syedshahmirsultan <115161841+syedshahmirsultan@users.noreply.github.com>
2023-04-18 20:47:51 +0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -6,16 +6,16 @@ export default function Home(){
   return(
     <div>
       <div className="h-[900px] w-2/2 bg-gray-900/10 pt-[30px] ">
-    <Link href="https://en.wikipedia.org/wiki/Arif_Alvi"><Image src="/images/president2.jpg" alt="picture of President" height={900} width={400}  className="mt-[40px] ml-[50px] "/></Link>
-      <h1 className="font-extrabold text-[85px] text-blue-800 ml-[600px] font-sans mt-[-650px]"><strong>Presidential Initiative</strong></h1>
-      <h1 className='text-[55px] text-blue-800 ml-[600px] mt-[30px]'>
+    <Link href="https://en.wikipedia.org/wiki/Arif_Alvi"><Image src="/images/president2.jpg" alt="picture of President" height={800} width={300}  className="mt-[40px] ml-[50px] "/></Link>
+      <h1 className="font-extrabold text-[65px] text-blue-800 ml-[600px] font-sans mt-[-650px]"><strong>Presidential Initiative</strong></h1>
+      <h1 className='text-[45px] text-blue-800 ml-[600px] mt-[30px]'>
 for Artificial Intelligence & Computing (PIAIC)</h1>
-<p className="text-[30px] text-blue-800 ml-[600px] mt-[50px] pr-[300px]">The mission of PIAIC is to reshape Pakistan by revolutionizing education, research, and business by adopting latest, cutting-edge technologies. Experts are calling this the 4th industrial revolution. We want Pakistan to become a global hub for AI, data science, cloud native computing, edge computing, blockchain, augmented reality, and internet of things.</p>
- <div className="mt-[130px] ml-[50px]"><Button><Link href="https://portal.piaic.org/signup">Enroll now</Link></Button></div> 
+<p className="text-[25px] text-blue-800 ml-[600px] mt-[20px] pr-[300px]">The mission of PIAIC is to reshape Pakistan by revolutionizing education, research, and business by adopting latest, cutting-edge technologies. Experts are calling this the 4th industrial revolution. We want Pakistan to become a global hub for AI, data science, cloud native computing, edge computing, blockchain, augmented reality, and internet of things.</p>
+ <div className="mt-[80px] ml-[50px]"><Button><Link href="https://portal.piaic.org/signup">Enroll now</Link></Button></div> 
  </div>
  <div className="h-[1200px] w-2/2">
   <h1 className=" mt-[50px] text-gray-700 text-[79px] text-center">Available Programs</h1>
-<Link href="/Artificial"> <div className="h-[300px] mt-[100px] w-[400px] bg-black-900 ml-[200px] border-2 border-blue-900 transition duration-150 ease-out transform hover:scale-125 hover:-translate-y-[50px]">
+<Link href="/Artificial"> <div className="h-[100px] mt-[100px] w-[200px] bg-black-900 ml-[200px] border-2 border-blue-900 transition duration-150 ease-out transform hover:scale-125 hover:-translate-y-[50px]">
  <div className="h-[50px] w-2/2 bg-blue-900">
     </div>
 <h1 className="text-[25px] text-center text-blue-900 mt-[30px] font-bold">Artificial Intelligence</h1>
@@ -49,7 +49,7 @@ for Artificial Intelligence & Computing (PIAIC)</h1>
 <div className=' mt-[300px] bg-teal-500 h-[750px] w-2/2'>
   <h1 className="text-white text-extrabold ml-[45px] text-[60px] font-serif">Image</h1>
   <Image src="/images/panapic.jpg" alt="picture of inaguration of PIAIC" height={800} width={900} className="ml-[50px] mt-[50px]"/>
-   <h1 className="text-black-900 text-[45px] pl-[1020px]  mt-[-530px] font-sans font-extrabold]">
+   <h1 className="text-black-900 text-[40px] pl-[1020px]  mt-[-530px] font-sans font-extrabold]">
    <strong> President launching PIAIC Website</strong> </h1>
  <p className="text-white text-[32px] text-left pl-[1020px] mt-[20px] pr-[20px]">President Dr. Arif Alvi launching the official website of PIAIC on December 9,3018.
   From left to right: Mr. Adil Altaf (Director Panacloud), Mr. Zia Ullah Khan (CEO Panacloud),
