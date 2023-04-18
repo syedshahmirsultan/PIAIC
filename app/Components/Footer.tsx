731d06f3a8a229a987dd4fd843d42fb6e2a46b04
syedshahmirsultan <115161841+syedshahmirsultan@users.noreply.github.com>
2023-04-18 21:29:49 +0500

    Update Footer.tsx

--- app/Components/Footer.tsx
+++ app/Components/Footer.tsx
@@ -2,9 +2,9 @@ import Image from 'next/image';
 import Link from 'next/link';
 export default function Footer(){
     return(
-        <div className="grid bg-gray-800 h-[100px] w-2/2 ">
-        <h1 className="text-white  mt-[2px] text-[30px] ml-[1420px]">Powered By</h1>
-       <Link href="https://web.facebook.com/groups/deep.learning.edu/?_rdc=1&_rdr"> <Image src="/images/panacloud.jpg" alt="panacloud" height={50} width={110} className="ml-[1460px]"/></Link>
+        <div className="grid bg-gray-800 h-[200px] w-2/2 ">
+        <h1 className="text-white  mt-[2px] text-[30px] ml-[1200px]">Powered By</h1>
+       <Link href="https://web.facebook.com/groups/deep.learning.edu/?_rdc=1&_rdr"> <Image src="/images/panacloud.jpg" alt="panacloud" height={50} width={110} className="ml-[1220px]"/></Link>
         </div>
     )
 }
