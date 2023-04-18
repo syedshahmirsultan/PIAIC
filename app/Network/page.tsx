04f41113dbdad8ee793d125f391147a02b80650e
syedshahmirsultan <115161841+syedshahmirsultan@users.noreply.github.com>
2023-04-18 23:10:23 +0500

    Update page.tsx

--- app/Network/page.tsx
+++ app/Network/page.tsx
@@ -4,9 +4,9 @@ export default function Network(){
     return(
         <div>
               <div className="h-[800px] w-2/2 bg-pink-600">
-        <h1 className="text-[85px] ml-[440px] text-white pt-[130px] font-extrabold">Network Programmability and Automation</h1>
-        <text className="pt-[10px] text-[25px] ml-[180px] text-white font-extrabold">A ONE YEAR PROGRAM OF NETWORK PROGRAMMABILITY AND AUTOMATION DESIGNED FOR ABSOLUTE BEGINNERS.</text>
-<br/><text className="text-[25px] ml-[300px] text-white font-extrabold">GETTING PAKISTAN READY FOR THE NEW ERA OF NETWORK PROGRAMMABILITY AND AUTOMATION.</text>
+        <h1 className="text-[75px] ml-[240px] text-white pt-[130px] font-extrabold">Network Programmability and Automation</h1>
+        <p className="pt-[10px] text-[25px] ml-[120px] text-white font-extrabold">A ONE YEAR PROGRAM OF NETWORK PROGRAMMABILITY AND AUTOMATION DESIGNED FOR ABSOLUTE BEGINNERS.</p>
+<br/><p className="text-[25px] ml-[200px] text-white font-extrabold">GETTING PAKISTAN READY FOR THE NEW ERA OF NETWORK PROGRAMMABILITY AND AUTOMATION.</p>
        <div className="ml-[-50px] mt-[150px]"><Button><Link href="https://portal.piaic.org/signup">Enroll now</Link></Button></div></div>
         </div>
     )
