ffc8e1a5d86149711e9c8b067452000e7220719b
syedshahmirsultan <115161841+syedshahmirsultan@users.noreply.github.com>
2023-04-18 21:45:23 +0500

    Update Button.tsx

--- app/Components/Button.tsx
+++ app/Components/Button.tsx
@@ -1,6 +1,7 @@
 const Button=({children}:any) => {
     return(
-        <button className='text-white text-center border-green-600  ml-[800px] bg-green-600 border-invisible border-2  h-[60px] w-[100px] rounded-lg p-[10px] mt-[-180px] transition ease-out animate-bounce hover:bg-green-900 hover:scale-150'>
+        <button className='text-white text-center border-green-600  ml-[600px] bg-green-600 border-invisible border-2  h-[60px] w-[100px]
+        rounded-lg p-[10px] mt-[-180px] transition ease-out animate-bounce hover:bg-green-900 hover:scale-150'>
         {children}</button>
     )
 
