3060c3b44036fe2b5468ede47da96f7b08f80436
syedshahmirsultan <115161841+syedshahmirsultan@users.noreply.github.com>
2023-04-19 05:34:11 +0500

    Update page.tsx

--- app/Web/page.tsx
+++ app/Web/page.tsx
@@ -3,9 +3,9 @@ import Button from '../Components/Button';
 export default function Web(){
     return(
         <div>  <div className="h-[800px] w-2/2 bg-purple-900">
-        <h1 className="text-[85px] ml-[440px] text-white pt-[130px] font-extrabold">Web 3.0 and Metaverse developer </h1>
-        <text className="pt-[10px] text-[25px] ml-[300px] text-white font-extrabold">A ONE YEAR PROGRAM OF WEB 3.0 AND METAVERSE DEVELOPMENT DESIGNED FOR ABSOLUTE BEGINNERS.</text>
-<br/><text className="text-[25px] ml-[300px] text-white font-extrabold">GETTING PAKISTAN READY FOR THE NEW ERA OF WEB 3.0 AND METAVERSE.</text>
+        <h1 className="text-[70px] ml-[340px] text-white pt-[130px] font-extrabold">Web 3.0 and Metaverse developer </h1>
+        <p className="pt-[10px] text-[25px] ml-[300px] text-white font-extrabold">A ONE YEAR PROGRAM OF WEB 3.0 AND METAVERSE DEVELOPMENT DESIGNED FOR ABSOLUTE BEGINNERS.</p>
+<br/><p className="text-[25px] ml-[300px] text-white font-extrabold">GETTING PAKISTAN READY FOR THE NEW ERA OF WEB 3.0 AND METAVERSE.</p>
        <div className="ml-[-50px] mt-[150px]"><Button><Link href="https://portal.piaic.org/signup">Enroll now</Link></Button></div></div>
 
         </div>
