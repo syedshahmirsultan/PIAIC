28aa0be373297cef5cf699e32429fd59226624da
syedshahmirsultan <115161841+syedshahmirsultan@users.noreply.github.com>
2023-04-20 00:20:32 +0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import Button from './Components/Button';
+import Footer from './Components/Footer';
 import Image from 'next/image';
 import Link from 'next/link';
 export default function Home(){
   return(
-    <div className="9000px">
+    <div>
       <div className="h-[800px] w-2/2 bg-gray-900/10 pt-[30px] ">
     <Link href="https://en.wikipedia.org/wiki/Arif_Alvi"><Image src="/images/president2.jpg" alt="picture of President" height={800} width={300}  className="mt-[40px] ml-[50px] "/></Link>
       <h1 className="font-extrabold text-[65px] text-blue-800 ml-[400px] font-sans mt-[-500px]"><strong>Presidential Initiative</strong></h1>
@@ -48,12 +49,12 @@ for Artificial Intelligence & Computing (PIAIC)</h1>
     </div>
 <h1 className="text-[25px] text-center text-pink-600 mt-[30px] font-bold">Network Programmability and Automation </h1>
   </div></Link></div>
-<div className=' mt-[200px] bg-teal-500 h-[750px] w-2/2'>
+<div className=' mt-[200px] bg-teal-500 h-[600px] w-2/2'>
   <h1 className="text-white text-extrabold ml-[45px] text-[60px] font-serif">Image</h1>
   <Image src="/images/panapic.jpg" alt="picture of inaguration of PIAIC" height={500} width={680} className="ml-[50px] mt-[50px]"/>
-   <h1 className="text-black-900 text-[45px] pl-[760px]  mt-[-450px] font-sans font-extrabold]">
+   <h1 className="text-black-900 text-[45px] pl-[760px]  mt-[10px] font-sans font-extrabold]">
    <strong> President launching PIAIC Website</strong> </h1>
- <p className="text-white text-[25px] text-left pl-[760px] mt-[20px] pr-[20px]">President Dr. Arif Alvi launching the official website of PIAIC on December 9,3018.
+ <p className="text-white text-[25px] text-left pl-[760px] mt-[5px] pr-[20px]">President Dr. Arif Alvi launching the official website of PIAIC on December 9,3018.
   From left to right: Mr. Adil Altaf (Director Panacloud), Mr. Zia Ullah Khan (CEO Panacloud),
   Mr. Hunaid Lakhani (Chancellor Iqra University), President Dr. Arif Alvi, Mr. Sulaiman S. Mehdi (Chairman Pakistan Stock  Exchange)
   , Mr. Zeeshan Hanif (CTO Panacloud), Maulana Bashir Farooqi
@@ -67,8 +68,7 @@ for Artificial Intelligence & Computing (PIAIC)</h1>
     className="ml-[200px] mt-[60px]"/></Link>
    <Link href="https://saylaniwelfare.com/en"><Image src="/images/saylani.jpg" height={200} width={500} alt="Logo of Saylani Welfare Trust" 
     className="ml-[600px] mt-[-150px]"/></Link>
-  
-</div>
+</div><Footer/>
  </div></div>
   
   )
