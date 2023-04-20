27aac2fe0ccd059c7d613771de8ea50d2c1d4106
syedshahmirsultan <115161841+syedshahmirsultan@users.noreply.github.com>
2023-04-21 01:26:01 +0500

    Update Footer.tsx

--- app/Components/Footer.tsx
+++ app/Components/Footer.tsx
@@ -4,8 +4,8 @@ export default function Footer(){
     return(
         <div className="grid bg-gray-800 h-[120px] w-2/2 ">
         <h1 className="text-white  mt-[2px] text-[30px] ml-[1190px]">Powered By</h1>
-       <Link href="https://web.facebook.com/groups/deep.learning.edu/?_rdc=1&_rdr"> <Image src="/images/panacloud.jpg" alt="panacloud" height={50} width={110}
-               className="ml-[1230px]"/></Link>
+       <Link href="https://web.facebook.com/groups/deep.learning.edu/?_rdc=1&_rdr"> <Image src="/images/panacloud.jpg" alt="panacloud" height={60} width={140}
+               className="ml-[1200px]"/></Link>
         </div>
     )
 }
